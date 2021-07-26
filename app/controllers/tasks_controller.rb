fedeefc0d5c6f2876c7fe9c1d957b1928a73a630
Alejandro Lopez Correa <Alejandro>
2021-07-26 11:15:22 -0500

    added link to add association to form with some corrections to the html structure

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,8 @@ class TasksController < ApplicationController
       @task.owner = current_user
       @task.save
       flash[:success] = "Object successfully created"
-      redirect_to @task
+      format.html{ redirect_to @task, notice: 'Task was successfully created.'}
+      format.json{ render :show, status: :created, location: @task }
     end    
   
     def update
