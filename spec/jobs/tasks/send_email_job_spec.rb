2179c869796e08fdbd438014714b62f913137c2d
Alejandro Lopez Correa <Alejandro>
2021-08-04 07:50:07 -0500

    added doubles spec

---
+++ spec/jobs/tasks/send_email_job_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Tasks::SendEmailJob, type: :job do
+  #test doubles
+  describe "#perform_async" do
+    let(:task_id){'1'}
+    #usar doubles
+    it 'send the email', focus: true do
+      task = class_double('Task').as_stubbed_const
+      service = double
+      object_double('Tasks::SendEmail', new: service).as_stubbed_const
+
+      expect(task).to receive(:find).with(task_id)
+      expect(service).to receive(:call)  
+
+
+      described_class.perform_async task_id
+    end
+  end
+  
+end
